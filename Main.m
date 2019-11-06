134bbdecbbcca16cdb7f373fe9945082a2e1cb7b
Tim Cardell <timcal04.04@gmail.com>
2019-11-06 15:13:27 +0100

    rot
    added rotation, not working

--- Main.m
+++ Main.m
@@ -1,8 +1,16 @@
 schemer_import('darksteel.prf')
 
 im = imread('db0_1.jpg');
-Translation(im);
-tnm034(im);
+%Translation(im);
+
+
+%Rotation(im, pointX, pointY, rotAngle)
+
+test = Rotation(im, 40, 40, 5);
+%tnm034(im);
 
 
 
+figure
+imshow(test)
+title('rot Image')
